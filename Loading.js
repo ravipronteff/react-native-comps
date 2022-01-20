a649ff726a6d092ce5dc6a7a126a38bcdca83dd2
ravipronteff <87562167+ravipronteff@users.noreply.github.com>
2022-01-20 14:50:55 +0530

    Create Loading.js

---
+++ Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  NetInfo,
+  Platform,
+} from "react-native";
+
+
+
+<View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator animating={true} />
+          <Text> hold on for a second</Text>
+  </View>
